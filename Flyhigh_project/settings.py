f10dd8de8c559c49067a93ae0f3b26235b2ffa90
Vvikram10 <vvikram3312@gmail.com>
2024-09-04 19:37:15 +0530

    allowed host modified

--- Flyhigh_project/settings.py
+++ Flyhigh_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['django-high-env.eba-fmnsu6pu.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
